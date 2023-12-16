274f5e38ae51086b50f68e6a8d311deea295d095
ntritin62 <21522677@gm.uit.edu.vn>
2023-12-16 23:15:49 +0700

    section-3-112

--- section-3-94/lib/meals.js
+++ section-3-94/lib/meals.js
@@ -4,6 +4,7 @@ import { resolve } from 'styled-jsx/css';
 const db = sql('meals.db');
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // throw new Error('Loading meals failed');
   return db.prepare('SELECT * FROM meals').all();
 }
