7baf89d39a8193fb3626db9829720de5ae5b9221
ntritin62 <21522677@gm.uit.edu.vn>
2023-12-19 01:13:07 +0700

    section-3-118

--- section-3-94/components/meals/image-picker.js
+++ section-3-94/components/meals/image-picker.js
@@ -39,6 +39,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
